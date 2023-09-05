a80d1241da27bc23c01de191ed6abb579f6e9245
thiagovieiras <thiago.santos@sptech.school>
2023-09-05 19:46:20 -0300

    Botão alterado, Resultado modicado e logo inserida

--- Sprint_1/script.js
+++ Sprint_1/script.js
@@ -7,5 +7,5 @@ function verificar() {
 
     caixa_resultado.style.display = "block"
     resposta.innerHTML = `
-    <span style="color: black; font-weight: 600;">Olá ${nome}, tudo bem? Você sabia que 25% dos clientes acabam desistindo da academia por falta de tempo, e outros 16% por conta da superlotação? Ou seja você pode vir a perder dentro dos proximos meses ${Math.trunc((clientes * 0.25) + (clientes * 0.16))}% dos clientes. Com nosso metodo Organum ${clientes} com nosso metodo de rotatividade autonoma</span>`
+    <span style="color: black; font-weight: 600;">Olá ${nome}, tudo bem? Você sabia que 25% dos clientes acabam desistindo da academia por falta de tempo, e outros 16% por conta da superlotação? Ou seja você pode vir a perder dentro dos proximos meses ${Math.trunc((clientes * 0.25) + (clientes * 0.16))}% dos clientes. Com nosso metodo TechGym você preservará 47% dos clientes dos quais tendem a desistir, e captará mais clientes pela qualidade de serviço.</span>`
 }
